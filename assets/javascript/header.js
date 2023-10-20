75413b442e19550d6566eb5054859ca1e13fc585
fiendgrin <yunisah@code.edu.az>
2023-10-20 22:07:00 +0400

    Slider Done

--- assets/javascript/header.js
+++ assets/javascript/header.js
@@ -13,7 +13,9 @@ accountHolder.addEventListener("click", (e) => {
   e.stopPropagation();
   chevronBasket.style.transform = "";
   basketContent.classList.remove("active");
-  navBar.style.display = "none";
+  if (window.innerWidth <= 768) {
+    navBar.style.display = "none";
+  }
   if (chevronAcc.style.transform == "rotate(-180deg)") {
     chevronAcc.style.transform = "";
     accountContent.classList.remove("active");
@@ -27,7 +29,9 @@ basketHolder.addEventListener("click", (e) => {
   e.stopPropagation();
   chevronAcc.style.transform = "";
   accountContent.classList.remove("active");
-  navBar.style.display = "none";
+  if (window.innerWidth <= 768) {
+    navBar.style.display = "none";
+  }
   if (chevronBasket.style.transform == "rotate(-180deg)") {
     chevronBasket.style.transform = "";
     basketContent.classList.remove("active");
@@ -55,7 +59,9 @@ window.addEventListener("click", () => {
   basketContent.classList.remove("active");
   chevronBasket.style.transform = "";
   chevronAcc.style.transform = "";
-  navBar.style.display = "none";
+  if (window.innerWidth <= 768) {
+    navBar.style.display = "none";
+  }
 });
 
 window.addEventListener("resize", () => {
---
+++ assets/javascript/swiper.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    speed: 1200,
+    spaceBetween: 200,
+  
+    autoplay:{
+        delay: 4000
+    },
+
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+ 
+  
+   
+  });
