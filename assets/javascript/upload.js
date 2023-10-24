4035d6b77efd9c8865049fa0b7772d05ba0b04db
fiendgrin <yunisah@code.edu.az>
2023-10-25 03:51:14 +0400

    Upload page 'General Information' section Done

---
+++ assets/javascript/upload.js
@@ -0,0 +1,140 @@
+let addBtn = document.querySelector("#uploadMain .addBtn");
+let inputField = document.querySelector("#uploadMain .tagsInputField");
+let tagHolder = document.querySelector("#uploadMain .tagHolder");
+let tagsAndBtn = document.querySelector("#uploadMain .tagsAndBtn");
+let inputTags = document.querySelectorAll("#uploadMain .singleTag");
+let theTags = document.querySelectorAll("#uploadMain .theTag");
+let inputIcon = document.querySelector("#uploadMain .inputIcon");
+if (inputTags.length >= 3) {
+  addBtn.setAttribute("disabled", "");
+}
+
+function inputCreator(tagToPass) {
+  return `<input class="singleTag" type="text" hidden value="${tagToPass
+    .toLowerCase()
+    .trim()}"/>`;
+}
+
+function tagCreator(tagToPass) {
+  return ` <div class="theTag">
+  ${tagToPass.toLowerCase().trim()}
+    <img class="close" id=${tagToPass
+      .toLowerCase()
+      .trim()
+      .replaceAll(" ", "#")} src="./assets/images/icons/plus.svg" alt="" />
+  </div>`;
+}
+
+inputField.addEventListener("input", () => {
+  inputIcon.textContent = `${inputField.value.length}/25`;
+});
+
+addBtn.addEventListener("click", (e) => {
+  let tagAlreadyExists = false;
+  if (inputField.value.length > 1 && inputField.value.length <= 25) {
+    if (inputTags.length > 0) {
+      inputTags.forEach((tags) => {
+        if (tags.value == inputField.value) {
+          tagAlreadyExists = true;
+        }
+      });
+      if (!tagAlreadyExists) {
+        tagHolder.innerHTML += inputCreator(inputField.value);
+        tagHolder.innerHTML += tagCreator(inputField.value);
+      }
+    } else if (inputTags.length == 0) {
+      tagHolder.innerHTML += inputCreator(inputField.value);
+      tagHolder.innerHTML += tagCreator(inputField.value);
+    }
+    inputField.value = "";
+    inputTags = document.querySelectorAll("#uploadMain .singleTag");
+    theTags = document.querySelectorAll("#uploadMain .theTag");
+    if (inputTags.length >= 3) {
+      addBtn.setAttribute("hidden", "");
+    }
+  }
+  let tagClose = document.querySelectorAll("#uploadMain .theTag .close");
+  tagClose.forEach((tc) => {
+    tc.addEventListener("click", () => {
+      inputTags.forEach((it) => {
+        theTags.forEach((tt) => {
+          if (
+            it.value == tc.id.replaceAll("#", " ") &&
+            tt.textContent.trim() == tc.id.replaceAll("#", " ")
+          ) {
+            it.remove();
+            tt.remove();
+            inputTags = document.querySelectorAll("#uploadMain .singleTag");
+            if (inputTags.length < 3) {
+              addBtn.removeAttribute("hidden");
+            }
+          }
+        });
+      });
+    });
+  });
+  if (inputTags.length >= 3) {
+    addBtn.setAttribute("hidden", "");
+  }
+  inputIcon.textContent = `${inputField.value.length}/25`;
+});
+
+let descs = document.querySelectorAll("#uploadMain .desc");
+let fileButtons = document.querySelectorAll("#uploadMain .fileUpload");
+let imgCover = document.querySelector("#uploadMain .coverImage");
+fileButtons.forEach((filebtn) => {
+  filebtn.addEventListener("change", () => {
+    let file = filebtn.files[0];
+    if (file) {
+      let fileName = file.name;
+      let lastDotIndex = fileName.lastIndexOf(".");
+      if (lastDotIndex !== -1) {
+        let fileExtension = fileName.substring(lastDotIndex + 1);
+        filebtn.previousElementSibling.textContent = `Your ${fileExtension.toUpperCase()} is acquired`;
+        filebtn.previousElementSibling.style.color = "white";
+        if (
+          fileExtension == "png" ||
+          fileExtension == "jpg" ||
+          fileExtension == "jpeg"
+        ) {
+          const reader = new FileReader();
+
+          reader.onload = function () {
+            const image = new Image();
+            image.src = reader.result;
+            image.onload = function () {
+              const canvas = document.createElement("canvas");
+              const ctx = canvas.getContext("2d");
+
+              const aspectRatio = 1;
+              const size = Math.min(image.width, image.height);
+              canvas.width = size;
+              canvas.height = size;
+
+              ctx.drawImage(
+                image,
+                (image.width - size) / 2,
+                (image.height - size) / 2,
+                size,
+                size,
+                0,
+                0,
+                size,
+                size
+              );
+
+              const croppedImage = new Image();
+              croppedImage.src = canvas.toDataURL("image/png");
+
+              imgCover.innerHTML = "";
+              imgCover.appendChild(croppedImage);
+            };
+          };
+
+          reader.readAsDataURL(file);
+        }
+      }
+      fileInput.value = "";
+    }
+  });
+});
