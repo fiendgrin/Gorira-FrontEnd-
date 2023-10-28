f4a1ccd765ae3337baa514b0c8b07988074a010b
fiendgrin <yunisah@code.edu.az>
2023-10-29 02:22:52 +0400

    Tracks Page Done and Fully Responsive

--- assets/javascript/tracks.js
+++ assets/javascript/tracks.js
@@ -22,7 +22,46 @@ numberInputs.forEach((numberInput) => {
 
 let sort = document.querySelector("#tracksMain .sort");
 let sortHolder = document.querySelector("#tracksMain .sortHolder");
+let left = document.querySelector("#tracksMain .left");
+let openFilters = document.querySelector("#tracksMain .openFilters");
 
-sort.addEventListener("click", () => {
+sort.addEventListener("click", (e) => {
+  e.stopPropagation();
   sortHolder.classList.toggle("active");
+  if (window.innerWidth < 768) {
+    left.classList.remove("active");
+  }
 });
+
+sortHolder.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+window.addEventListener("click", (e) => {
+  sortHolder.classList.remove("active");
+});
+
+// let allInputs = document.querySelectorAll("input");
+
+// allInputs.forEach((i) => {
+//   i.addEventListener("change", () => {
+//     console.log(i.value);
+//     console.log(i.checked);
+//   });
+// });
+
+if (window.innerWidth < 768) {
+  openFilters.addEventListener("click", (e) => {
+    e.stopPropagation();
+    left.classList.toggle("active");
+    sortHolder.classList.remove("active");
+  });
+  left.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+
+  window.addEventListener("click", (e) => {
+    left.classList.remove("active");
+    sortHolder.classList.remove("active");
+  });
+}
