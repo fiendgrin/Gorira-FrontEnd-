08272a65c62cf03a75e8213777117f594b9090d0
fiendgrin <yunisah@code.edu.az>
2023-11-05 18:52:52 +0400

    Messanger Fully Responsive

--- assets/javascript/messanger.js
+++ assets/javascript/messanger.js
@@ -2,3 +2,31 @@ let meaasagingBox = document.querySelector(".meaasagingBox");
 
 meaasagingBox.scrollTop =
   meaasagingBox.scrollHeight - meaasagingBox.clientHeight;
+
+let left = document.querySelector("#messangerMain .left");
+let people = document.querySelector("#messangerMain .people");
+
+people.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (left.style.display != "flex") {
+    left.style.display = "flex";
+  } else {
+    left.style.display = "none";
+  }
+});
+
+left.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+window.addEventListener("click", () => {
+  left.style.display = "none";
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    left.style.display = "flex";
+  } else {
+    left.style.display = "none";
+  }
+});
